a5e9c6fa85793a4522fbe56b4bb06b23d6cfdfc1
Moringa <ephantusmwangi864@gmail.com>
2018-06-05 10:46:33 +0300

    Initial project setup

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Documents/zawadi/pythone/LMS-Project/Zjango/Practice/TheSocialNetwork/virtual/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
